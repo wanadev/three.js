6171cda93548bd05ae0c2e15ea5a92a9bf2ae569
Benjamin <bguignabert@wanadev.fr>
2013-04-26 10:12:58 +0200

    r58

--- build/three.js
+++ build/three.js
@@ -3,7 +3,7 @@
  * @author Larry Battle / http://bateru.com/news
  */
 
-var THREE = THREE || { REVISION: '58dev' };
+var THREE = THREE || { REVISION: '58' };
 
 self.console = self.console || {
 
@@ -34157,6 +34157,8 @@ THREE.CameraHelper = function ( camera ) {
 	this.matrixAutoUpdate = false;
 
 	this.pointMap = pointMap;
+
+	this.update();
 
 };
 
